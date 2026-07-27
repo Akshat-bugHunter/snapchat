ebc718ded7fe1d8e6b860ad447a60baa866aeb2d
Akshat <saxenaakshat219@gmail.com>
2026-07-27 22:41:30 +0530

    show friends

--- core/views.py
+++ core/views.py
@@ -3,8 +3,9 @@ from django.views.decorators.http import require_http_methods
 from django.contrib.auth import login, logout, get_user_model
 
 from django.contrib.auth.decorators import login_required
-# from django.db.models import Q
-# from .models import  Message
+from django.db.models import Q
+
+from .models import Message, FriendRequest
 from . import forms
 
 # from django.db import IntegrityError
@@ -46,9 +47,36 @@ def logout_view(request):
 def home(request):
     return render(request, "pages/chat.html")
 
+
 @login_required
 def search_view(request):
+    unique_friends = []
+    sent = []
+    received = []
     User = get_user_model()
-    query = request.GET.get("q","")
+    query = request.GET.get("q", "")
     users = User.objects.filter(username__icontains=query).exclude(id=request.user.id)
-    return render(request, "pages/search.html", {"users": users})
+
+    queryset = FriendRequest.objects.filter(
+        Q(from_user=request.user) | Q(to_user=request.user)
+    )
+    friends = queryset.filter(status=FriendRequest.StatusChoice.ACCEPTED)
+    pending_request = queryset.filter(status=FriendRequest.StatusChoice.PENDING)
+
+    for friend in friends:
+        if request.user == friend.from_user:
+            unique_friends.append(friend.to_user.id)
+        else:
+            unique_friends.append(friend.from_user.id)
+
+    for req in pending_request:
+        if request.user == req.from_user:
+            sent.append(req.to_user.id)
+        else:
+            received.append(req.from_user.id)
+
+    return render(
+        request,
+        "pages/search.html",
+        {"users": users, "friends": unique_friends, "sent": sent, "received": received},
+    )
